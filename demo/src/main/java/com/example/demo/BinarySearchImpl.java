d463a1d1213fea8f482bfac5fdc836d374789530
Chitra Chaudhari <chitraksonawane@gmail.com>
2023-04-12 13:25:35 -0400

    Spring framework demo

---
+++ demo/src/main/java/com/example/demo/BinarySearchImpl.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BinarySearchImpl {
+	
+	@Autowired
+	private SortAlgorithm sortAlgorithm;
+	
+	/*public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
+		super();
+		this.sortAlgorithm = sortAlgorithm;
+	}*/
+	
+	/*public SortAlgorithm getSortAlgorithm() {
+		return sortAlgorithm;
+	}
+
+	public void setSortAlgorithm(SortAlgorithm sortAlgorithm) {
+		this.sortAlgorithm = sortAlgorithm;
+	}*/
+
+	public int binarySearch(int[] numbers, int numberToSearchFor) {
+		//Sorting n array
+		int[] sortedNumbers = sortAlgorithm.sort(numbers);
+		System.out.println(sortAlgorithm);
+		//Search the array
+		//Return the Result
+		return 3;
+	}
+
+}
